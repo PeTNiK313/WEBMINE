f365a05d649ac7a289d2dba810e296e20653d777
PeTNiK313 <nekit31333@yandex.ru>
2021-04-11 22:07:40 +0500

    first commit

---
+++ Laba4/urls.py
@@ -0,0 +1,42 @@
+"""
+Definition of urls for Laba4.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+from django.conf.urls import url
+admin.autodiscover()
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('login/',
+         LoginView.as_view
+         (
+             template_name='app/login.html',
+             authentication_form=forms.BootstrapAuthenticationForm,
+             extra_context=
+             {
+                 'title': 'Авторизация',
+                 'year' : datetime.now().year,
+             }
+         ),
+         name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('admin/', admin.site.urls),
+    url(r'^anketa$', views.anketa, name='anketa'),
+    url(r'^registration$', views.registration, name='registration'),
+    url(r'^newpost$', views.newpost, name='newpost'),
+    url(r'^videopost$', views.videopost, name='videopost'),
+    url(r'^blog', views.blog, name='blog'),
+    url(r'^(?P<parametr>\d+)/$', views.blogpost, name='blogpost'),
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
